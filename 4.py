4221dc0fc620ee99cc6922667efcfc624e64cc54
flashmaks <72061883+flashmaks@users.noreply.github.com>
2024-02-03 16:58:42 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,13 +1,13 @@
 import random
-f = open('scientist.txt', encoding='UTF8').read()
+f = open('scientist.txt', encoding='UTF8').read()  #Считываем, а после преобразовываем файл для использования
 f = f.split('\n')
 m = []
 for i in f:
     m.append(i.split('#'))
-def login(x):
+def login(x): #Создаем логин
     x = x.split(' ')
     return x[0] + '_' + x[1][0] + x[2][0]
-def password():
+def password(): #Создаем пароль
     Alphabet1 = 'QWERTYUIOPASDFGHJKLZXCVBNM'
     Alphabet2 = 'qwertyuiopasdfghjklzxcvbnm'
     Alphabet3 = '0123456789'
@@ -22,10 +22,10 @@ def password():
     c1 = Alphabet3[random.randint(0, len(Alphabet3) - 1)]
     c2 = Alphabet3[random.randint(0, len(Alphabet3) - 1)]
     return a1 + a2 + a3 + a4 + b1 + b2 + b3 + b4 + c1 + c2
-for i in m:
+for i in m: 
     i.append(login(i[0]))
     i.append(password())
 f = open('scientist_password.csv', 'w', encoding='UTF8')
-for i in m:
+for i in m: #Записываем в файл
     f.write('#'.join(i) + '\n')
 
